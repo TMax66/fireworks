111cec1a478e4e4ec0ce9f4946c5b9d45d7e1e13
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2020-10-23 09:52:24 +0200

    update

--- scripts/code.r
+++ scripts/code.r
@@ -1,3 +1,13 @@
 library("here")
 library("tidyverse")
+library("readxl")
+library("DataExplorer")
 
+# df <- read_excel(sheet = "Sheet1", here("data", "raw", "Equine anxiety_dataset.xlsx"))
+# df <- filter(df, df$`Unusual behavior`== "Yes")
+# 
+# saveRDS(df, here("data", "processed",  file = "dati.rds"))
+
+df <- readRDS(here("data", "processed",  "dati.rds"))
+
+create_report(df)
