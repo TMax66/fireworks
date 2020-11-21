a7ef6a2ebe5ddeb1510ac7b00d7e0172c8dc1f78
Massimo Tranquillo <tmax@Air-di-Massimo.lan>
2020-11-21 11:15:00 +0100

    update

--- scripts/code.r
+++ scripts/code.r
@@ -1,6 +1,8 @@
 library("here")
 library("tidyverse")
 library("readxl")
+library("FactoMineR")
+library("factoextra")
 library("DataExplorer")
 
 # df <- read_excel(sheet = "Sheet1", here("data", "raw", "Equine anxiety_dataset.xlsx"))
@@ -10,4 +12,19 @@ library("DataExplorer")
 
 df <- readRDS(here("data", "processed",  "dati.rds"))
 
-create_report(df)
+d_MCA <- df %>% 
+  select(6:12, 21, 25, 26) %>% 
+  na.omit()
+
+#create_report(d_MCA)
+
+
+res.mca = MCA(d_MCA, graph = FALSE)
+
+eig.val <- get_eigenvalue(res.mca)
+
+fviz_screeplot(res.mca, addlabels = TRUE, ylim = c(0, 45))
+
+fviz_mca_biplot(res.mca, 
+                repel = TRUE, # Avoid text overlapping (slow if many point)
+                ggtheme = theme_minimal())
