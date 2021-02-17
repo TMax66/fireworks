8fe724a7b2642728506b984591edc0edf70c4f34
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-02-17 09:24:36 +0100

    update

--- scripts/code.r
+++ scripts/code.r
@@ -11,26 +11,24 @@ library("DataExplorer")
 # saveRDS(df, here("data", "processed",  file = "dati.rds"))
 
 df <- readRDS(here("data", "processed",  "dati.rds"))
+plot_bar(df)### seleziono solo le variabili "discriminanti"...elimino quelle in cui c'è poca variabilità nelle risposte alle diverse categorie della variabile####
 
 d_MCA <- df %>% 
-  filter(Country=="US") %>% 
-  select(6:17, 20, 21, 25, 26, 28:30,35, 36:44) %>% 
-  mutate(across(where(is.character), as.factor))
-  na.omit()  
+  #filter(Country=="US") %>% 
+  select(2, 6:9, 10:14,  16:17, 20, 25, 26, 28:30, 36:38, 40:41, 43:44) %>% 
+  mutate(across(where(is.character), as.factor)) %>% 
+  na.omit()
 
 ##non utilizzabili le colonne 18,19,22, 23, 24, 27, 28(?-verificare se usarla o meno), 33 (da usare come variabile di raggruppamento), 
 ##45:50--------------------
 
 #create_report(d_MCA)
 
-summary(d_MCA)[, 1:31]
 
-for (i in 1:31) {
-  plot(d_MCA[,i], main=colnames(d_MCA)[i],
-       ylab = "Count", col="steelblue", las = 2)
-}
 
-res.mca = MCA(d_MCA, graph = FALSE)
+ 
+
+res.mca = MCA(d_MCA[, -1], graph = FALSE)
 
 eig.val <- get_eigenvalue(res.mca)
 
